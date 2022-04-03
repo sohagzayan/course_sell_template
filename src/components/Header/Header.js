934ea2a9ddccf9a4de52410010feaffa2452ab76
sohagmia360 <gdsohag360@gmail.com>
2022-04-03 11:33:25 +0600

    fixx m p

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,70 +1,81 @@
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
 import logo from "../../assets/image/logoUdamy.png";
 import { useDataContext } from "../../context/GetDataContext";
 const Header = () => {
+  const value = useDataContext();
+  const { card, setCard } = value;
+  const [headerScroll, setHeaderScroll] = useState(false);
 
-  const value = useDataContext()
-  const {card , setCard}  = value
-  const [headerScroll , setHeaderScroll] = useState(false)
-
-
-  const headerScrollColorChnage = ()=>{
-    const scrolly = window.scrollY
+  const headerScrollColorChnage = () => {
+    const scrolly = window.scrollY;
     console.log(scrolly);
-    if(scrolly > 120){
-      console.log('right time');
-      setHeaderScroll(true)
-    }else{
-      console.log('time not ');
-      setHeaderScroll(false)
+    if (scrolly > 120) {
+      console.log("right time");
+      setHeaderScroll(true);
+    } else {
+      console.log("time not ");
+      setHeaderScroll(false);
     }
-  }
+  };
 
-  useEffect(()=>{
-    window.addEventListener('scroll' , headerScrollColorChnage)
-  },[])
+  useEffect(() => {
+    window.addEventListener("scroll", headerScrollColorChnage);
+  }, []);
 
   return (
-    <motion.div   animate={ headerScroll ?  '': {scale : 1}}  transition={{ duration:3 }} className={headerScroll ? "  w-full  mx-auto  mt-3 sticky top-0  bg-secondary_white shadow-lg transition-all ease-linear duration-200" : " transition-all ease-linear duration-150  w-full mx-auto container mt-3  transform scale-50 "}>
-    <div className=' flex justify-between items-center container mx-auto'>
-    <div className="w-32">
-        <img src={logo} alt="logo" />
+    <motion.div
+      animate={headerScroll ? "" : { scale: 1 }}
+      transition={{ duration: 3 }}
+      className={
+        headerScroll
+          ? "  w-full  mx-auto  mt-3 sticky top-0  bg-secondary_white shadow-lg transition-all ease-linear duration-200 py-2 px-4"
+          : " transition-all ease-linear duration-150  w-full mx-auto container mt-3  transform scale-50 px-4"
+      }
+    >
+      <div className=" flex justify-between items-center container mx-auto">
+        <div className="w-32">
+          <img src={logo} alt="logo" />
+        </div>
+        <ul className="menuList flex items-center  gap-5 text-dark_blue font-semibold tracking-wider">
+          <li className="">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "text-light_tomato" : ""
+              }
+              to="/"
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="relative ">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "text-light_tomato" : ""
+              }
+              to="/card"
+            >
+              Card
+            </NavLink>
+            {card.length ? (
+              <small className="absolute -top-3 -right-3 bg-light_tomato w-6 h-6 flex items-center justify-center rounded-full text-white">
+                {card.length}
+              </small>
+            ) : null}
+          </li>
+          <li className=" ">
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? "text-light_tomato" : ""
+              }
+              to="/feedback"
+            >
+              Feedback
+            </NavLink>
+          </li>
+        </ul>
       </div>
-      <ul className="menuList flex items-center  gap-5 text-dark_blue font-semibold tracking-wider">
-        <li className="">
-          <NavLink
-            className={({ isActive }) => (isActive ? "text-light_tomato" : "")}
-            to="/"
-          >
-            Home
-          </NavLink>
-        </li>
-        <li className="relative ">
-          <NavLink
-            className={({ isActive }) => (isActive ? "text-light_tomato" : "")}
-            to="/card"
-          >
-            Card
-          </NavLink>
-         {
-           card.length ?  <small className="absolute -top-3 -right-3 bg-light_tomato w-6 h-6 flex items-center justify-center rounded-full text-white">
-           {card.length }
-            </small>
-         : null
-         }
-        </li>
-        <li className=" ">
-          <NavLink
-            className={({ isActive }) => (isActive ? "text-light_tomato" : "")}
-            to="/feedback"
-          >
-            Feedback
-          </NavLink>
-        </li>
-      </ul>
-    </div>
     </motion.div>
   );
 };
--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaSearch } from 'react-icons/fa';
 import { useDataContext } from '../../context/GetDataContext';
 
 
@@ -9,10 +10,10 @@ const SearchForm = () => {
     const {searchKeyWord , setSearchKeyWord} = useDataContext()
 
     return (
-        <div className='flex items-center justify-center mt-10'>
-            <form className='md:w-3/6 w-6/6 bg-secondary_white shadow-md flex items-center rounded-md  py-1 px-4'>
+        <div className='flex items-center justify-center mt-10 px-10 '>
+            <form className='md:w-3/6 w-full  bg-secondary_white shadow-md flex  items-center rounded-md  py-3 '>
                 <input value={searchKeyWord} onChange={(e)=> setSearchKeyWord(e.target.value)} className='w-full font-medium bg-transparent text-light_tomato px-3 border-0 outline-none' type="text" placeholder='Search' />
-                    <span className='bg-dark_blue px-4 text-white rounded-md cursor-pointer'>Search</span>
+                    <span className=' px-4 text-dark_blue text-xl  cursor-pointer'><FaSearch /></span>
             </form>
         </div>
     );
