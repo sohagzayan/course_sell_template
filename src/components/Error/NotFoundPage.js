082c1d4ef59985db0e6abcba2156030a338664ea
sohagmia360 <gdsohag360@gmail.com>
2022-04-03 18:55:41 +0600

    fix some bugg

--- src/components/Error/NotFoundPage.js
+++ src/components/Error/NotFoundPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
-
+import { useNavigate } from 'react-router-dom';
 const NotFoundPage = () => {
+    const navigate = useNavigate()
     return (
         <div className='flex flex-col justify-center items-center h-screen'>
             <h2>Not Found Page</h2>
             <h4>This page not here</h4>
+            <button onClick={()=> navigate('/')} className='bg-dark_blue text-white px-3 py-1 rounded mt-5'>Go Home</button>
         </div>
     );
 };
